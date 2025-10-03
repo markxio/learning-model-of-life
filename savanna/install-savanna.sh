9ed1234d4446cf51a15414388de453e1d1b9b2eb
markxio <m.klaisoongnoen@epcc.ed.ac.uk>
2025-10-03 17:33:05 +0000

    cleanup

--- savanna/install-savanna.sh
+++ savanna/install-savanna.sh
@@ -33,27 +33,3 @@ pip install numpy
 pip install ninja
 pip install hatchling
 pip install --global-option="build_ext" --global-option="-j32" flash-attn==2.6.3 --no-build-isolation
-
-# up to here tested
-
-python -m pip install --upgrade pip
-pip install pipenv
-pipenv install "setuptools==71.1.0"
-pipenv sync --system --dev
-
-apt-get install python3.11-dev
-
-git clone --recurse-submodules https://github.com/markxio/evo2
-cd evo2
-
-pip install --use-pep517 --no-build-isolation .
-
-# I was getting KeyError: 'recipe', 
-# but after pip uninstall transformer-engine (removing 2.1.0) 
-# and then running pip install transformer_engine[pytorch]==1.13
-# it was fixed for me.
-pip uninstall transformer_engine
-pip install transformer_engine[pytorch]==1.13
-
-python ./test/test_evo2.py --model_name evo2_7b
-#python ./test/test_evo2.py --model_name evo2_40b
